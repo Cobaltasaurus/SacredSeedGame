5686b0524a82a0c645bfade1602d5b97c5fa4fb8
Cobaltasaurus <gm.blues@gmail.com>
2018-07-30 13:27:43 +0000

    First commit

---
+++ commands/command.py
@@ -0,0 +1,282 @@
+"""
+Commands
+
+Commands describe the input the account can do to the game.
+
+"""
+
+from evennia import Command as BaseCommand
+# from evennia import default_cmds
+
+
+class Command(BaseCommand):
+    """
+    Inherit from this if you want to create your own command styles
+    from scratch.  Note that Evennia's default commands inherits from
+    MuxCommand instead.
+
+    Note that the class's `__doc__` string (this text) is
+    used by Evennia to create the automatic help entry for
+    the command, so make sure to document consistently here.
+
+    Each Command implements the following methods, called
+    in this order (only func() is actually required):
+        - at_pre_cmd(): If this returns True, execution is aborted.
+        - parse(): Should perform any extra parsing needed on self.args
+            and store the result on self.
+        - func(): Performs the actual work.
+        - at_post_cmd(): Extra actions, often things done after
+            every command, like prompts.
+
+    """
+    pass
+
+# -------------------------------------------------------------
+#
+# The default commands inherit from
+#
+#   evennia.commands.default.muxcommand.MuxCommand.
+#
+# If you want to make sweeping changes to default commands you can
+# uncomment this copy of the MuxCommand parent and add
+#
+#   COMMAND_DEFAULT_CLASS = "commands.command.MuxCommand"
+#
+# to your settings file. Be warned that the default commands expect
+# the functionality implemented in the parse() method, so be
+# careful with what you change.
+#
+# -------------------------------------------------------------
+
+# from evennia.utils import utils
+#
+#
+# class MuxCommand(Command):
+#     """
+#     This sets up the basis for a MUX command. The idea
+#     is that most other Mux-related commands should just
+#     inherit from this and don't have to implement much
+#     parsing of their own unless they do something particularly
+#     advanced.
+#
+#     Note that the class's __doc__ string (this text) is
+#     used by Evennia to create the automatic help entry for
+#     the command, so make sure to document consistently here.
+#     """
+#     def has_perm(self, srcobj):
+#         """
+#         This is called by the cmdhandler to determine
+#         if srcobj is allowed to execute this command.
+#         We just show it here for completeness - we
+#         are satisfied using the default check in Command.
+#         """
+#         return super(MuxCommand, self).has_perm(srcobj)
+#
+#     def at_pre_cmd(self):
+#         """
+#         This hook is called before self.parse() on all commands
+#         """
+#         pass
+#
+#     def at_post_cmd(self):
+#         """
+#         This hook is called after the command has finished executing
+#         (after self.func()).
+#         """
+#         pass
+#
+#     def parse(self):
+#         """
+#         This method is called by the cmdhandler once the command name
+#         has been identified. It creates a new set of member variables
+#         that can be later accessed from self.func() (see below)
+#
+#         The following variables are available for our use when entering this
+#         method (from the command definition, and assigned on the fly by the
+#         cmdhandler):
+#            self.key - the name of this command ('look')
+#            self.aliases - the aliases of this cmd ('l')
+#            self.permissions - permission string for this command
+#            self.help_category - overall category of command
+#
+#            self.caller - the object calling this command
+#            self.cmdstring - the actual command name used to call this
+#                             (this allows you to know which alias was used,
+#                              for example)
+#            self.args - the raw input; everything following self.cmdstring.
+#            self.cmdset - the cmdset from which this command was picked. Not
+#                          often used (useful for commands like 'help' or to
+#                          list all available commands etc)
+#            self.obj - the object on which this command was defined. It is often
+#                          the same as self.caller.
+#
+#         A MUX command has the following possible syntax:
+#
+#           name[ with several words][/switch[/switch..]] arg1[,arg2,...] [[=|,] arg[,..]]
+#
+#         The 'name[ with several words]' part is already dealt with by the
+#         cmdhandler at this point, and stored in self.cmdname (we don't use
+#         it here). The rest of the command is stored in self.args, which can
+#         start with the switch indicator /.
+#
+#         This parser breaks self.args into its constituents and stores them in the
+#         following variables:
+#           self.switches = [list of /switches (without the /)]
+#           self.raw = This is the raw argument input, including switches
+#           self.args = This is re-defined to be everything *except* the switches
+#           self.lhs = Everything to the left of = (lhs:'left-hand side'). If
+#                      no = is found, this is identical to self.args.
+#           self.rhs: Everything to the right of = (rhs:'right-hand side').
+#                     If no '=' is found, this is None.
+#           self.lhslist - [self.lhs split into a list by comma]
+#           self.rhslist - [list of self.rhs split into a list by comma]
+#           self.arglist = [list of space-separated args (stripped, including '=' if it exists)]
+#
+#           All args and list members are stripped of excess whitespace around the
+#           strings, but case is preserved.
+#         """
+#         raw = self.args
+#         args = raw.strip()
+#
+#         # split out switches
+#         switches = []
+#         if args and len(args) > 1 and args[0] == "/":
+#             # we have a switch, or a set of switches. These end with a space.
+#             switches = args[1:].split(None, 1)
+#             if len(switches) > 1:
+#                 switches, args = switches
+#                 switches = switches.split('/')
+#             else:
+#                 args = ""
+#                 switches = switches[0].split('/')
+#         arglist = [arg.strip() for arg in args.split()]
+#
+#         # check for arg1, arg2, ... = argA, argB, ... constructs
+#         lhs, rhs = args, None
+#         lhslist, rhslist = [arg.strip() for arg in args.split(',')], []
+#         if args and '=' in args:
+#             lhs, rhs = [arg.strip() for arg in args.split('=', 1)]
+#             lhslist = [arg.strip() for arg in lhs.split(',')]
+#             rhslist = [arg.strip() for arg in rhs.split(',')]
+#
+#         # save to object properties:
+#         self.raw = raw
+#         self.switches = switches
+#         self.args = args.strip()
+#         self.arglist = arglist
+#         self.lhs = lhs
+#         self.lhslist = lhslist
+#         self.rhs = rhs
+#         self.rhslist = rhslist
+#
+#         # if the class has the account_caller property set on itself, we make
+#         # sure that self.caller is always the account if possible. We also create
+#         # a special property "character" for the puppeted object, if any. This
+#         # is convenient for commands defined on the Account only.
+#         if hasattr(self, "account_caller") and self.account_caller:
+#             if utils.inherits_from(self.caller, "evennia.objects.objects.DefaultObject"):
+#                 # caller is an Object/Character
+#                 self.character = self.caller
+#                 self.caller = self.caller.account
+#             elif utils.inherits_from(self.caller, "evennia.accounts.accounts.DefaultAccount"):
+#                 # caller was already an Account
+#                 self.character = self.caller.get_puppet(self.session)
+#             else:
+#                 self.character = None
+
+# file sacredseed/commands/command.py
+# this is our dice roller lol
+
+from evennia import default_cmds
+from evennia import Command
+import random
+from collections import Counter
+
+class Cmd_Roll(default_cmds.MuxCommand):
+        """
+        Prototype of the games roller.
+
+        Usage:
+          roll <# of dice>,<# of difficulty>
+
+        Please god let this work....
+        """
+
+        key = "roll"
+        aliases =["check, dice"]
+        locks = "cmd:all()"
+        help_category = "General Commands"
+
+        def func(self):
+                caller = self.caller
+                location = caller.location
+                num = int(self.lhslist[0])
+                diff = int(self.lhslist[1])
+                successes, rolls = self.roll(num, diff)
+                location.msg_contents("|y<Roll>|n %s gained |350%s successes|n at difficulty |y%s|n. |222%s|n" % (str(caller), str(successes), str(diff), str(rolls)))
+
+        def roll(self, num, diff):
+                rolls = [random.randint(1, 12) for x in range(num)]
+                successes = 0
+                c = Counter(rolls)
+                for roll, times in c.items():
+                        if roll >= diff:
+                                successes += times
+                                continue
+                        successes += (roll*times)//diff
+#               print("That's %s successes" % str(successes))
+                return successes, rolls
+
+
+class CmdSheet(Command):
+
+    """
+    Shows your sheet to you.
+
+    Usage:
+      sheet
+
+    Aliases:
+      stats
+
+
+    Displays a list of your current ability values.
+    """
+
+    key = "sheet"
+    aliases = ["stats"]
+    lock = "cmd:all()"
+    help_category = "General Commands"
+
+    def func(self):
+        "implements the actual functionality"
+
+        soma, fight, panache = self.caller.get_active_attr()
+        vim, defend, wisdom = self.caller.get_passive_attr()
+        influence, research, create, athletics, larceny, sail = self.caller.get_active_skills()
+        integrity, knowledge, resistance, awareness, insight, navigation = self.caller.get_passive_skills()
+        brawl, shoot, throw, melee, mount = self.caller.get_combat_skills()
+        mana, luck = self.caller.get_spec_traits()
+
+
+        name = self.caller.name
+	name_disp = "{ Sheet: %s }" % name
+	hdr_length = 78-len(name_disp)
+        header = name_disp + '-'*hdr_length
+        attr_block ="{ATTRS}\n\t\t|gActive\t\tPassive\n|gPhysical|n\t\tSoma:    %s\t\tVim:    %s\n|gCombat|n\t\tFight:   %s\t\tDefend: %s\n|gAbstract|n\t\tPanache: %s\t\tWisdom: %s" % (soma, vim, fight, defend, panache, wisdom)
+        skill_block = "\n\n{SKILLS}\n|gActive\t\tPassive\t\tCombat|n\nInfluence:  %s\t\tIntegrity:  %s\t\tBrawl:      %s" \
+                      "\nResearch:   %s\t\tKnowledge:  %s\t\tShoot:      %s" \
+                      "\nCreate:     %s\t\tResistance: %s\t\tThrow:      %s" \
+                      "\nAthletics:  %s\t\tAwareness:  %s\t\tMelee:      %s" \
+                      "\nLarceny:    %s\t\tInsight:    %s\t\tMount:      %s" \
+                      "\nSail:       %s\t\tNavigation: %s\n\n" % (influence, integrity, brawl, research, knowledge, shoot, create, resistance, throw, athletics, awareness, melee, larceny, insight, mount, sail, navigation)
+
+        spec_block = "|mMana|n: %s\t\t|yLuck|n: %s" % (mana, luck)
+        self.caller.msg(header)
+	self.caller.msg(attr_block)
+	self.caller.msg(skill_block)
+	self.caller.msg(spec_block)
+
+#	skills = evtable.EvTable("|gActive", " ", table=[["Influence", "Research", "Create", "Athletics", "Larceny", "Sail"], [influence, research, create, athletics, larceny, sail]])
+
+
---
+++ typeclasses/characters.py
@@ -0,0 +1,96 @@
+"""
+Characters
+
+Characters are (by default) Objects setup to be puppeted by Accounts.
+They are what you "see" in game. The Character class in this module
+is setup to be the "default" character type created by the default
+creation commands.
+
+"""
+from evennia import DefaultCharacter
+
+
+class Character(DefaultCharacter):
+    """
+    The Character defaults to reimplementing some of base Object's hook methods with the
+    following functionality:
+
+    at_basetype_setup - always assigns the DefaultCmdSet to this object type
+                    (important!)sets locks so character cannot be picked up
+                    and its commands only be called by itself, not anyone else.
+                    (to change things, use at_object_creation() instead).
+    at_after_move(source_location) - Launches the "look" command after every move.
+    at_post_unpuppet(account) -  when Account disconnects from the Character, we
+                    store the current location in the pre_logout_location Attribute and
+                    move it to a None-location so the "unpuppeted" character
+                    object does not need to stay on grid. Echoes "Account has disconnected"
+                    to the room.
+    at_pre_puppet - Just before Account re-connects, retrieves the character's
+                    pre_logout_location Attribute and move it back on the grid.
+    at_post_puppet - Echoes "AccountName has entered the game" to the room.
+
+    """
+
+    def at_object_creation(self):
+
+        # default attributes set when the character is first created.
+
+        # all players start with 1 in their attributes
+        # active attributes
+        self.db.active_attr= {'Soma': 1, 'Fight': 1, 'Panache': 1}
+
+        #passive attributes
+        self.db.passive_attr = {'Vim': 1, 'Defend': 1, 'Wisdom': 1}
+
+        #players start with 0 in their skills
+        #active skills
+        self.db.active_skill={'Influence': 0, 'Research': 0, 'Create': 0, 'Athletics': 0, 'Larceny': 0, 'Sail': 0}
+
+        #passive skills
+        self.db.passive_skill = {'Integrity': 0, 'Knowledge': 0, 'Resistance': 0, 'Awareness': 0, 'Insight': 0, 'Navigation': 0}
+
+
+        #combat skills
+        self.db.combat_skill = {'Brawl': 0, 'Shoot': 0, 'Throw': 0, 'Melee': 0, 'Mount': 0}
+
+
+        #traits are things that can be spent
+        #special things players start with 5 luck 10 mana
+        self.db.special_trait= {'Luck': 5, 'Mana': 10}
+
+    def get_active_attr(self):
+        """simple method to get active attris as a tuple (some, fight, panache"""
+
+        return self.db.active_attr['Soma'], self.db.active_attr['Fight'], self.db.active_attr['Panache']
+
+
+    def get_passive_attr(self):
+        """simple method to get passive attrs as a tuple (vim, defend, wisdom"""
+
+        return self.db.passive_attr['Vim'], self.db.passive_attr['Defend'], self.db.passive_attr['Wisdom']
+
+
+    def get_active_skills(self):
+        """simple method to get active skills as a long ass
+        tuple (influence, research, create, athletics, larceny, sail)"""
+
+        return self.db.active_skill['Influence'], self.db.active_skill['Research'], self.db.active_skill['Create'], self.db.active_skill['Athletics'], self.db.active_skill['Larceny'], self.db.active_skill['Sail']
+
+    def get_passive_skills(self):
+        """simple method to get passive skills as a long ass
+        tuple (integrity, knowledge, resistance, awareness, insight, navigation)"""
+
+        return self.db.passive_skill['Integrity'], self.db.passive_skill['Knowledge'], self.db.passive_skill['Resistance'], self.db.passive_skill['Awareness'], self.db.passive_skill['Insight'], self.db.passive_skill['Navigation']
+
+    def get_combat_skills(self):
+        """simple method to get combat skills in a long ass tuple
+        (brawll, shoot, throw, melee, mount"""
+
+        return self.db.combat_skill['Brawl'], self.db.combat_skill['Shoot'], self.db.combat_skill['Throw'], self.db.combat_skill['Melee'], self.db.combat_skill['Mount']
+
+    def get_spec_traits(self):
+        """simple method to get special traits as a tuple (mana, luck)"""
+
+        return self.db.special_trait['Mana'], self.db.special_trait['Luck']
+    
+
